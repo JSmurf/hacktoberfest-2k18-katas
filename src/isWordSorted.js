be00da9e5b717c7120b8823bf2c9544e6c10ea12
Alex Cooper <mrsketchygalore@me.com>
2018-10-10 15:13:09 -0400

    Implement isWordSorted
    Create a function to check if a word is sorted.

--- src/isWordSorted.js
+++ src/isWordSorted.js
@@ -1,3 +1,16 @@
-export const isWordSorted = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const isWordSorted = (str) => {
+  var string = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  var checkAscending = 1;
+  var checkDescending = 1;
+  for (var i = 0; i < (string.length - 1); i++){
+    if (string[i + 1] > string[i]){
+      checkAscending++;
+    }else if(string[i + 1] < string[i]){
+      checkDescending++;
+    }else if(string[i + 1] == string[i]){
+      checkAscending++;
+      checkDescending++;
+    }
+  }
+  return (checkDescending == string.length || checkAscending == string.length);
 };
